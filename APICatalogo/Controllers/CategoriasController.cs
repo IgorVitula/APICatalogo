4ad8a0282faf1d7757f2a72dbfe26bd5a8d1bb00
Igor zaffalon <igorzaffalon@hotmail.com>
2025-08-07 17:59:50 -0300

    ##Subindo classe context vamos ver
    Add AppDbContext for database management
    
    Introduces the `AppDbContext` class inheriting from `DbContext` to manage the application's database context. The class includes a constructor for `DbContextOptions<AppDbContext>` and defines two `DbSet` properties: `Categorias` for `Categoria` entities and `produtos` for `Produto` entities.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,84 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.ToList();
+
+            if (categorias is null || !categorias.Any())            
+                return NotFound("Nenhuma categoria encontrada.");  
+            
+            return categorias;
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}", Name = "ObterCategoriaPorId")]
+        public ActionResult<Categoria> GetId(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+            if(categoria is null)
+                return NotFound("Categoria não encontrada.");
+
+            return categoria;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria) {
+
+            if(categoria is null)
+                return BadRequest("Categoria inválida.");
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return null;
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+                return BadRequest("Categoria inválida.");
+
+            _context.Entry(categoria).State = EntityState.Modified; 
+            _context.SaveChanges();
+
+            return Ok(categoria);
+
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ActionResult Delete (int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+            if (categoria == null)
+                return BadRequest("Nenhuma categoria encontrada.");
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+    }
+}
---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,83 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.produtos.ToList();
+            if (produtos is null )            
+                return NotFound();
+            
+            return produtos;
+        }
+
+        [HttpGet]
+        [Route("{id:int}", Name ="ObterProduto")]
+        public ActionResult<Produto> ObterPorId(int id)
+        {
+            var produto = _context.produtos.FirstOrDefault(x => x.ProdutoId == id);
+            if (produto is null)            
+                return NotFound("Produto Não encontrado");
+            
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if(produto is null)
+            {
+                return BadRequest("Produto inválido");
+            }
+
+            _context.produtos.Add(produto);
+            _context.SaveChanges(); // persiste os dados na tabela 
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto); // aqui nesse retorno ele ja chama a rota de cima com o id do novo protudo pra vermos o retorno dele criado
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public ActionResult put(int id, Produto produto) // put é atualização completa
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest("Produto inválido");
+            }
+            _context.Entry(produto).State = EntityState.Modified; // informa que o produto foi modificado
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.produtos.FirstOrDefault(x => x.ProdutoId == id);
+            if (produto is null)
+            {
+                return BadRequest("Produto Não encontrado");
+            }
+            _context.produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
